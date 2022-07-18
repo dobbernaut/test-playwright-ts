f194b82a5f0d9a098f7520dfde7410560c642c4d
noelsevilla <noel.sev@gmail.com>
2022-07-19 11:30:40 +1200

    Update env fetch errors to warn from the console

--- config/config.ts
+++ config/config.ts
@@ -20,7 +20,7 @@ const getPassword = (): string => {
   if (process.env.testpassword) {
     return process.env.testpassword;
   } else {
-    throw new Error('No password was set for the test role. Add environment variable testpassword');
+    console.warn('No password was set for the test role. Add environment variable testpassword');
   }
 };
 
@@ -28,7 +28,7 @@ const getUserName = (): string => {
   if (process.env.testuser) {
     return process.env.testuser;
   } else {
-    throw new Error('No username was set for the test role. Add environment variable username');
+    console.warn('No username was set for the test role. Add environment variable username');
   }
 };
 
@@ -43,7 +43,7 @@ const getToken = (): string => {
   if (process.env.token) {
     return process.env.token;
   } else {
-    throw new Error('No oauthToken was set for client. Add environment variable token');
+    console.warn('No oauthToken was set for client. Add environment variable token');
   }
 };
 
@@ -51,7 +51,7 @@ const getTokenSecret = (): string => {
   if (process.env.tokensecret) {
     return process.env.tokensecret;
   } else {
-    throw new Error('No oauthTokenSecret was set for the client. Add environment variable tokensecret');
+    console.warn('No oauthTokenSecret was set for the client. Add environment variable tokensecret');
   }
 };
 
@@ -59,7 +59,7 @@ const getKey = (): string => {
   if (process.env.key) {
     return process.env.key;
   } else {
-    throw new Error('No consumerKey was set for the client. Add environment variable key');
+    console.warn('No consumerKey was set for the client. Add environment variable key');
   }
 };
 
@@ -67,7 +67,7 @@ const getKeySecret = (): string => {
   if (process.env.keysecret) {
     return process.env.keysecret;
   } else {
-    throw new Error('No consumerSecret was set for the client. Add environment variable keysecret');
+    console.warn('No consumerSecret was set for the client. Add environment variable keysecret');
   }
 };
 
